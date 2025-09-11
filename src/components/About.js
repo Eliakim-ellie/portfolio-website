72bd00c86266a8c4d16de9cf139121c74d782927
silent_debugger <walelaeliakim123@gmail.com>
2025-09-11 06:50:00 +0300

    Add About section with personal introduction

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+function About() {
+  return (
+    <>
+      <section className="about">
+        <h2>About Me</h2>
+        <p>
+          I’m Silent Debugger, a beginner React developer passionate about building web apps
+          and learning new technologies. This portfolio is my first real React project.
+        </p>
+        <p>
+          My journey started with HTML, CSS, and JavaScript, and now I’m exploring the power
+          of React to create modern, interactive websites.
+        </p>
+      </section>
+    </>
+  );
+}
+
+export default About;
